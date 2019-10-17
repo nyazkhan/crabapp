8ea9b438e0de594195ccf9330413982d711f2436
nyazkhan <khannyaz1705@gmail.com>
2019-10-17 11:09:29 +0530

    firebase and google addresh

--- src/app/pages/googlemap/googlemap.page.ts
+++ src/app/pages/googlemap/googlemap.page.ts
@@ -1,5 +1,11 @@
 import { Component, NgZone, ElementRef, ViewChild, Inject, AfterViewInit, OnInit } from '@angular/core';
 import { Geolocation, GeolocationOptions, Geoposition, PositionError } from '@ionic-native/geolocation/ngx';
+import { PopoverController, LoadingController } from '@ionic/angular';
+import { CommanService } from 'src/app/service/comman.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+// import { PopoverPage } from './address-popover/address-popover';
+// import { RestaurentAddress } from 'src/app/class/restaurent';
 declare const google: any;
 @Component({
   selector: 'app-googlemap',
@@ -18,15 +24,46 @@ export class GooglemapPage implements OnInit {
   places;
   infoWindow;
   markers: any = [];
-  autocomplete;
+
   MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
+  address: any = {};
+  // = {
+  //   address: '',
+  //   locality: '',
+  //   landMark: '',
+  //   state: '',
+  //   city: '',
+  //   distic: '',
+  //   country: '',
+  //   pincode: null,
+  // };
 
+  addressForm;
 
-
+  componentForm = {
+    street_number: 'long_name',
+    sublocality_level_1: 'long_name',
+    sublocality_level_2: 'long_name',
+    sublocality_level_3: 'long_name',
+    route: 'long_name',
+    locality: 'long_name',
+    administrative_area_level_1: 'long_name',
+    country: 'long_name',
+    postal_code: 'short_name'
+  };
+  restaurentDetails = [];
 
   input: any;
   constructor(
-    @Inject(Geolocation) public geolocation: Geolocation) { }
+    @Inject(Geolocation) public geolocation: Geolocation,
+    public popoverCtrl: PopoverController,
+    public loadingController: LoadingController,
+    @Inject(CommanService) public commanService: CommanService,
+    @Inject(AngularFirestore) public firestore: AngularFirestore,
+    @Inject(Router) private router: Router,
+
+  ) {
+  }
 
   // tslint:disable-next-line: use-lifecycle-interface
   async ngAfterViewInit() {
@@ -45,9 +82,11 @@ export class GooglemapPage implements OnInit {
         // scaleControl: false,
         fullscreenControl: false
       };
+
       this.initAutocomplete();
     }, (err) => {
       console.log(err);
+
     });
 
 
@@ -79,10 +118,12 @@ export class GooglemapPage implements OnInit {
     // Listen for the event fired when the user selects a prediction and retrieve
     // more details for that place.
     searchBox.addListener('places_changed', () => {
-      const places = searchBox.getPlaces();
-      console.log(places);
+      this.restaurentDetails = [];
 
-      if (places.length == 0) {
+      const places = searchBox.getPlaces();
+      this.restaurentDetails = places[0];
+      console.log(places);
+      if (places.length === 0) {
         return;
       }
 
@@ -94,6 +135,85 @@ export class GooglemapPage implements OnInit {
 
       // For each place, get the icon, name and location.
       const bounds = new google.maps.LatLngBounds();
+
+
+      // Get each component of the address from the place details,
+      // and then fill-in the corresponding field on the form.
+
+      for (let i = 0; i < places[0].address_components.length; i++) {
+        const addressType = places[0].address_components[i].types[0];
+        if (this.componentForm[addressType]) {
+          console.log(places[0].address_components[i][this.componentForm[addressType]]);
+
+          if (addressType === 'postal_code') {
+            this.address.pincode = places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+
+
+          if (addressType === 'country') {
+            this.address.country = places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+
+
+
+          if (addressType === 'administrative_area_level_1') {
+            this.address.state = places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+
+
+
+          if (addressType === 'locality') {
+            this.address.distic = places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+
+          if (addressType === 'street_number') {
+            this.address.address = places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+          if (addressType === 'route') {
+            this.address.landMark = places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+
+          if (addressType === 'neighborhood') {
+            this.address.landMark += '  ' + places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+          if (addressType === 'sublocality_level_3') {
+            this.address.locality = places[0].address_components[i][this.componentForm[addressType]] || '';
+
+          }
+
+
+
+          if (addressType === 'sublocality_level_2') {
+            this.address.locality += '  ' + places[0].address_components[i][this.componentForm[addressType]];
+
+          }
+
+
+
+          if (addressType === 'sublocality_level_1') {
+            this.address.city = places[0].address_components[i][this.componentForm[addressType]];
+
+          }
+
+        }
+      }
+      this.address.address = places[0].vicinity;
+
+      console.log(this.address);
+      console.log(this.restaurentDetails);
+
+
+
+
+
+
       places.forEach((place) => {
         if (!place.geometry) {
           console.log('Returned place contains no geometry');
@@ -128,228 +248,57 @@ export class GooglemapPage implements OnInit {
           bounds.extend(place.geometry.location);
         }
       });
+      this.changeMapHieght('50vh');
+
       this.map.fitBounds(bounds);
     });
-  }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  loadMap() {
-
-    this.geolocation.getCurrentPosition().then((position) => {
-
-      this.latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-      console.log(position);
-
-      this.mapOptions = {
-        center: this.latLng,
-        zoom: 14,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-
-      };
-      this.initMap();
-      // googleMaps.event.addListenerOnce(map, 'idle', () => {
-      //     mapEle.classList.add('show-map');
-      //   });
-      // this.initAutocomplete();
-    }, (err) => {
-      console.log(err);
-    });
 
   }
 
-
-  initMap() {
-    this.map = new google.maps.Map(this.mapElement.nativeElement, this.mapOptions);
-
-
-
-    this.autocomplete = new google.maps.places.Autocomplete(
-        // tslint:disable-next-line: no-redundant-jsdoc
-        /** @type {!HTMLInputElement} */(
-        document.getElementById('autocomplete'), { type: ['(cities)'] }));
-    // console.log(this.autocomplete.getPlace());
-
-    this.autocomplete.setComponentRestrictions({ 'country': 'in' });
-    this.autocomplete.addListener('place_changed', () => {
-      // this.infowindow.close();
-      // this.marker.setVisible(false);
-      this.clearMarkers();
-      this.createMarker(this.autocomplete.getPlace());
-    });
-    // this.autocomplete.setFields(['address_component']);
-
-    this.places = new google.maps.places.PlacesService(this.map);
-    // this.search();
-
-    // Add a DOM event listener to react when the user selects a country.
-    // document.getElementById('country').addEventListener(
-    //   'change', this.setAutocompleteCountry);
-  }
   ngOnInit() {
   }
 
-  // onPlaceChanged() {
-  //   const place = this.autocomplete.getPlace();
-  //   console.log(place);
 
-  //   if (place.geometry) {
-  //     this.map.panTo(place.geometry.location);
-  //     this.map.setZoom(15);
-  //     this.search();
-  //   } else {
-  //     // document.getElementById('autocomplete').placeholder = 'Enter a city';
-  //   }
-  // }
+  changeMapHieght(val) {
 
-  // search() {
-  //   console.log(this.latLng);
-  //   const search = {
-  //     location: this.latLng,
+    (document.getElementById('map') as HTMLInputElement).style.height = val;
 
-  //     radius: 5500,
-  //     // bounds: this.map.getBounds(),
-  //     types: ['restaurant', 'bar', 'cafe', 'casino', 'bakery']
-  //   };
-
-  //   this.places.nearbySearch(search, (results, status) => {
-
-  //     this.callback(results, status);
-  //     // if (status === google.maps.places.PlacesServiceStatus.OK) {
-  //     //   this.clearResults();
-  //     //   this.clearMarkers();
-  //     //   // Create a marker for each hotel found, and
-  //     //   // assign a letter of the alphabetic to each marker icon.
-  //     //   for (let i = 0; i < results.length; i++) {
-  //     //     const markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-  //     //     const markerIcon = this.MARKER_PATH + markerLetter + '.png';
-  //     //     // Use marker animation to drop the icons incrementally on the map.
-  //     //     this.markers[i] = new google.maps.Marker({
-  //     //       position: results[i].geometry.location,
-  //     //       animation: google.maps.Animation.DROP,
-  //     //       icon: markerIcon
-  //     //     });
-  //     //     // If the user clicks a hotel marker, show the details of that hotel
-  //     //     // in an info window.
-  //     //     this.markers[i].placeResult = results[i];
-  //     //     google.maps.event.addListener(this.markers[i], 'click', this.showInfoWindow());
-  //     //     setTimeout(this.dropMarker(i), i * 100);
-  //     //     this.addResult(results[i], i);
-  //     //   }
-  //     // }
-  //   });
-  // }
-
-
-  // callback(results, status) {
-  //   if (status === google.maps.places.PlacesServiceStatus.OK) {
-  //     for (let i = 0; i < results.length; i++) {
-  //       this.createMarker(results[i]);
-  //     }
-  //   }
-  // }
-
-  createMarker(place) {
-    const placeLoc = place;
-    console.log('placeLoc', placeLoc);
-    this.markers = new google.maps.Marker({
-      map: this.map,
-      position: place.geometry.location
-    });
-
-    this.infoWindow = new google.maps.InfoWindow();
-
-    google.maps.event.addListener(this.markers, 'click', () => {
-      // this.ngZone.run(() => {
-      this.infoWindow.setContent(place.name);
-      this.infoWindow.open(this.map, this.markers);
-    });
-    // });
   }
 
 
 
 
-  clearMarkers() {
-    console.log('elete marker');
 
-    for (let i = 0; i < this.markers.length; i++) {
-      if (this.markers[i]) {
-        this.markers[i].setMap(null);
-      }
-    }
-    this.markers = [];
+
+
+  gotoWelcomPage(val) {
+    console.log(val.value);
+    // tslint:disable-next-line: no-string-literal
+    this.restaurentDetails['customAddress'] = this.address;
+    JSON.parse(JSON.stringify(this.restaurentDetails));
+    this.firestore.collection('Restaurent').add(JSON.parse(JSON.stringify(this.restaurentDetails))).then((docRef) => {
+      console.log('Document written with ID: ', docRef.id);
+      this.router.navigate(['/welcome'], { queryParams: { restaurent: docRef.id } });
+
+    }).catch((error) => {
+      // this.alertService.showInfoAlert('Error adding document: ');
+      console.log(error);
+
+    });
+    // }
   }
 
 
-  // dropMarker(i) {
-  //   return () => { this.markers[i].setMap(this.map); };
-  // }
 
-  // addResult(result, i) {
-  //   const results = document.getElementById('results');
-  //   const markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-  //   const markerIcon = this.MARKER_PATH + markerLetter + '.png';
 
-  //   const tr = document.createElement('tr');
-  //   tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
-  //   tr.onclick = () => {
-  //     // google.maps.event.trigger(this.markers[i], 'click');
-  //   };
 
-  //   const iconTd = document.createElement('td');
-  //   const nameTd = document.createElement('td');
-  //   const icon = document.createElement('img');
-  //   icon.src = markerIcon;
-  //   icon.setAttribute('class', 'placeIcon');
-  //   icon.setAttribute('className', 'placeIcon');
-  //   const name = document.createTextNode(result.name);
-  //   iconTd.appendChild(icon);
-  //   nameTd.appendChild(name);
-  //   tr.appendChild(iconTd);
-  //   tr.appendChild(nameTd);
-  //   results.appendChild(tr);
-  // }
 
-  // clearResults() {
-  //   const results = document.getElementById('results');
-  //   while (results.childNodes[0]) {
-  //     results.removeChild(results.childNodes[0]);
-  //   }
-  // }
 
-  // // Get the place details for a hotel. Show the information in an info window,
-  // // anchored on the marker for the hotel that the user selected.
-  // showInfoWindow() {
-  //   const marker: any = this;
-  //   this.places.getDetails({ placeId: marker.placeResult.place_id },
-  //     function (place, status) {
-  //       if (status !== google.maps.places.PlacesServiceStatus.OK) {
-  //         return;
-  //       }
-  //       this.infoWindow.open(this.map, marker);
-  //       this.buildIWContent(place);
-  //     });
-  // }
+
+
+
+
+
+
 
 }
